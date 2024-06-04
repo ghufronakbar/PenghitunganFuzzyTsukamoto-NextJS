a6451a7251de8c504245d87e0c9505c6a05888a8
ghufron akbar <alan.040219.gam@gmail.com>
2024-06-04 19:30:34 +0700

    Sidebar Color

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -3,16 +3,20 @@ import { useRouter } from "next/router";
 import { Menu, MenuItem, Sidebar, SubMenu } from "react-pro-sidebar";
 import { useQuery } from "@tanstack/react-query";
 import axiosInstanceAuthorization from "@/lib/axiosInstanceAuthorization";
-import { secondaryColor } from "@/lib/color";
+import { primaryColor, secondaryColor, white } from "@/lib/color";
 import Image from "next/image";
 
 export function SidebarMenu() {
   const router = useRouter();
 
-  const { data: profileSB, isLoading: loadingProfileSB, refetch: refetchProfileSB } = useQuery({
+  const {
+    data: profileSB,
+    isLoading: loadingProfileSB,
+    refetch: refetchProfileSB,
+  } = useQuery({
     queryKey: ["profileSB"],
     queryFn: async () => {
-      const { data } = await axiosInstanceAuthorization.get("/profile");      
+      const { data } = await axiosInstanceAuthorization.get("/profile");
       return data.user;
     },
   });
@@ -21,38 +25,41 @@ export function SidebarMenu() {
     localStorage.removeItem("token");
     router.push("/login");
   };
-  
+
   return (
     <>
-      
-        <Sidebar>
-          <br />
-          <Box
-            p={3}
-            mx={2}
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-          >
-            <Stack onClick={() => router.push(`/profile`)}>
-             
-              <Text as="b" fontSize="2xl" color={secondaryColor} textAlign="center">
-                {profileSB && (profileSB.fullname)}
-              </Text>
-            </Stack>
-          </Box>
-          <br />
-          <br />
-          <Menu>
-            <MenuItem onClick={() => router.push(`/calculate`)}>🧮 Kalkulasi</MenuItem>
-            <MenuItem onClick={() => router.push(`/history`)}>📒 Riwayat</MenuItem>           
-            <MenuItem onClick={handleLogout}>🔒 Logout</MenuItem>
-          </Menu>
-        </Sidebar>
-      
+      <Sidebar backgroundColor={primaryColor}>
+        <br />
+        <Box
+          p={3}
+          mx={2}
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Stack onClick={() => router.push(`/profile`)}>
+            <Text as="b" fontSize="2xl" color={white} textAlign="center">
+              {profileSB && profileSB.fullname}
+            </Text>
+          </Stack>
+        </Box>
+        <br />
+        <br />
+        <Menu>
+          <MenuItem onClick={() => router.push(`/calculate`)}>
+            <Text color={white}>🧮 Kalkulasi</Text>
+          </MenuItem>
+          <MenuItem onClick={() => router.push(`/history`)}>
+            <Text color={white}>📒 Riwayat</Text>
+          </MenuItem>
+          <MenuItem onClick={handleLogout}>
+            <Text color={white}>🔒 Logout</Text>
+          </MenuItem>
+        </Menu>
+      </Sidebar>
     </>
   );
 }
